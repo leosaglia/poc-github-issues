eaf868fdd35f793418d8069f4aa7c2d461dcbca6
Leonardo Saglia <leosaglia@hotmail.com>
2023-03-21 22:13:37 -0300

    a

---
+++ src/update_yaml.py
@@ -0,0 +1,11 @@
+import yaml
+import os
+
+# get the input and convert it to int
+
+def read_and_modify_one_block_of_yaml_data(filename=teste, key=execucao.mensagem, value="message"):
+    with open(f'{filename}.yaml', 'r') as f:
+        data = yaml.safe_load(f)
+        data[f'{key}'] = f'{value}' 
+        print(data) 
+    print('done!')
