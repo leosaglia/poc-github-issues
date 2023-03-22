5ae37346592cd5229fc8e4a0acb32ef95a9ce8d9
Leonardo Saglia <leosaglia@hotmail.com>
2023-03-22 11:23:43 -0300

    separar action

--- src/update_yaml.py
+++ src/update_yaml.py
@@ -4,7 +4,7 @@ import os
 # get the input and convert it to int
 
 def read_and_modify_one_block_of_yaml_data(filename='teste', key='["execucao"]["mensagem"]'):
-    num = os.environ.get("INPUT_ABRANGENCIA")
+    num = os.environ.get("INPUT_MARKS")
 
     with open(f'{filename}.yaml', 'w') as f:
         data = yaml.safe_load(f)
