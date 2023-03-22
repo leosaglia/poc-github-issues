eb7a2b0303720db4080ff9e3203e68e2a8a3899a
Leonardo Saglia <leosaglia@hotmail.com>
2023-03-21 22:37:41 -0300

    a

--- src/update_yaml.py
+++ src/update_yaml.py
@@ -7,7 +7,6 @@ def read_and_modify_one_block_of_yaml_data(filename='teste', key='["execucao"]["
     with open(f'{filename}.yaml', 'r') as f:
         data = yaml.safe_load(f)
         data["execucao"]["mensagem"] = f'{value}' 
-        yaml.dump(data,f,sort_keys=False)
         print(data) 
     with open(f'teste2.yaml', 'w') as file:
         yaml.dump(data,file,sort_keys=False)
