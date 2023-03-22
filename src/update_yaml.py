b671504c04f977a7bb7776e69ac0eb7101b326e8
Leonardo Saglia <leosaglia@hotmail.com>
2023-03-22 13:02:43 -0300

    a

--- src/update_yaml.py
+++ src/update_yaml.py
@@ -7,11 +7,11 @@ def read_and_modify_one_block_of_yaml_data():
     marks = os.environ.get("INPUT_MARKS")
     ambiente = os.environ.get("INPUT_AMBIENTE")
 
-    novo_comando = f'        
+    novo_comando = '''        
       - echo "Executando os testes no ambiente de DEV"
       - cd tests/integrations
       - python -m pytest -M "{marks}" -E {ambiente.lower()} --junitxml=relatorio.xml || true
-    '
+    '''
 
     with open(f'teste.yaml', 'r+') as f:
         data = yaml.safe_load(f)
