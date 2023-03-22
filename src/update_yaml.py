b73e29ca942f3d01302e9a3484b44544131a0ddc
Leonardo Saglia <leosaglia@hotmail.com>
2023-03-21 23:43:34 -0300

    A

--- src/update_yaml.py
+++ src/update_yaml.py
@@ -3,10 +3,12 @@ import os
 
 # get the input and convert it to int
 
-def read_and_modify_one_block_of_yaml_data(filename='teste', key='["execucao"]["mensagem"]', value="message"):
+def read_and_modify_one_block_of_yaml_data(filename='teste', key='["execucao"]["mensagem"]'):
+    num = os.environ.get("INPUT_ABRANGENCIA")
+
     with open(f'{filename}.yaml', 'w') as f:
         data = yaml.safe_load(f)
-        data["execucao"]["mensagem"] = f'{value}' 
+        data["execucao"]["mensagem"] = num 
         yaml.dump(data,f,sort_keys=False)
         print(data) 
     print('done!')
