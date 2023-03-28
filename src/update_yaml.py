f5d14b3b1302f5cc906ecdf68b2df6eec299fa10
Leonardo Saglia <leosaglia@hotmail.com>
2023-03-28 11:54:51 -0300

    removando f da formatacao de string

--- src/update_yaml.py
+++ src/update_yaml.py
@@ -11,10 +11,10 @@ def read_and_modify_one_block_of_yaml_data():
 
     yaml_data = None
 
-    with open(f'tests/teste.yaml', 'r') as f:
+    with open('tests/teste.yaml', 'r') as f:
         yaml_data = yaml.safe_load(f)
 
-    with open(f'tests/teste.yaml', 'w') as file:
+    with open('tests/teste.yaml', 'w') as file:
         yaml_data["phases"]["build"]["commands"][-1] = novo_comando 
         yaml.dump(yaml_data,file, sort_keys=False)
 
